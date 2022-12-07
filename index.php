c8d40ab082b9f9308afcf337280a38872d79651b
PattiyaMora <119998593+PattiyaMora@users.noreply.github.com>
2022-12-07 15:18:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
-   <?php echo '<p>My name is Itthiphol</p>'; ?> 
+ <?php echo '<p>Hello World</p>'; ?>  
+   <?php echo '<p>My name is Pattiya</p>'; ?>
      <?php echo '<p>Update-3st</p>'; ?> 
      <?php echo '<p>Update-4st</p>'; ?> 
   <?php echo '<p>Update-555555555555st</p>'; ?> 
